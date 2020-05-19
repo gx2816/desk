bf0f6592cfa643b092a8fce770a893cfdcb95047
gaozw <gaozw@venpoo.com>
2020-05-19 10:48:58 +0800

    Updates

---
+++ precache-manifest.ab8ebc7a97f230b4b5b91bcd5deed1ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ce17307b7855e263ab98a20ce08c91",
+    "url": "/desk/index.html"
+  },
+  {
+    "revision": "0e4bc286433a20c99d03",
+    "url": "/desk/static/css/2.99b2bda0.chunk.css"
+  },
+  {
+    "revision": "c1e082f402d404c79bc9",
+    "url": "/desk/static/css/main.c94782a0.chunk.css"
+  },
+  {
+    "revision": "0e4bc286433a20c99d03",
+    "url": "/desk/static/js/2.0368cdf2.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/desk/static/js/2.0368cdf2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1e082f402d404c79bc9",
+    "url": "/desk/static/js/main.ee189141.chunk.js"
+  },
+  {
+    "revision": "f434ed3bfa7383564825",
+    "url": "/desk/static/js/runtime-main.ff3edf76.js"
+  },
+  {
+    "revision": "a568162c2e16b799f5d56634d9e23e46",
+    "url": "/desk/static/media/bg.a568162c.svg"
+  }
+]);
